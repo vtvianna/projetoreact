67d40acc1ca1f8b648dc581d1fa06d6aca479e64
victor <vtviannac@gmail.com>
2025-09-17 14:45:21 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+import Sobre from './components/pages/Sobre'
+import Contact from './components/pages/Contact'
+import Projects from './components/pages/Projects'
+
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
+
+
+
